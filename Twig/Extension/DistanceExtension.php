235ba58c1801f28e67c14cf51cba08515cc157b3
Philippe Gamache <info@ph-il.ca>
2014-06-09 12:50:35 -0400

    Add DistanceExtension

---
+++ Twig/Extension/DistanceExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Phil\GeolocationBundle\Twig\Extension;
+
+use League\Geotools\AbstractGeotools;
+use League\Geotools\Distance\Distance;
+
+class DistanceExtension extends \Twig_Extension
+{
+    /**
+     * @var Distance
+     */
+    public $distance;
+
+    /**
+     * @param Distance $distance
+     */
+    public function __construct(Distance $distance)
+    {
+        $this->distance = $distance;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return array(
+            'distance' => new \Twig_Filter_Method($this, 'distance')
+        );
+    }
+
+    public function address(Point $from, Point $to, $flags = AbstractGeotools::KILOMETER_UNIT)
+    {
+        return $this->distance->setFrom($from)->setTo($to)->in($flags)->vincenty();
+    }
+
+    /**
+     * Name of this extension
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'distance';
+    }
+}
